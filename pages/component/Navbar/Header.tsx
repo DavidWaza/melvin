cd1ddf05f294e8b31c4f45645c451da155d4812f
David Waza <wazabanga00@gmail.com>
2023-11-11 22:54:40 +0100

    created desktop menu

--- pages/component/Navbar/Header.tsx
+++ pages/component/Navbar/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header>
       <div>
-        <Container className="flex justify-between gap-5 h-[6rem] p-10 border-b border-slate-200 font-medium font-lexend">
+        <Container className="flex justify-between gap-5 h-[6rem] p-10 border-b border-slate-200 font-medium font-lexend uppercase">
           <Link href="/" className="logo">
             Logo
           </Link>
--- pages/component/Navbar/Navbar.tsx
+++ pages/component/Navbar/Navbar.tsx
@@ -4,11 +4,10 @@ import React, { useState } from "react";
 const Navbar = () => {
   return (
     <nav>
-      <ul className="flex items-center gap-5">
-        <li>Home</li>
-        <div className="group">
+      <ul className="flex items-center gap-5 uppercase">
+      <div className="group">
           <button className=" px-3 py-1 flex items-center min-w-32">
-            <span className="pr-1 font-semibold flex-1">Dropdown</span>
+            <span className="pr-1 font-semibold flex-1 uppercase">Products for none-residents</span>
             <span>
               <svg
                 className="fill-current h-4 w-4 transform group-hover:-rotate-180
@@ -25,12 +24,12 @@ const Navbar = () => {
   transition duration-150 ease-in-out origin-top min-w-32"
           >
             <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-              Programming
+              For investors
             </li>
-            <li className="rounded-sm px-3 py-1 hover:bg-gray-100">DevOps</li>
+            <li className="rounded-sm px-3 py-1 hover:bg-gray-100">for representative officers</li>
             <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
               <button className="w-full text-left flex items-center outline-none focus:outline-none">
-                <span className="pr-1 flex-1">Langauges</span>
+                <span className="pr-1 flex-1 uppercase">for grantors</span>
                 <span className="mr-auto">
                   <svg
                     className="fill-current h-4 w-4
@@ -48,37 +47,45 @@ const Navbar = () => {
   min-w-32
   "
               >
-                <li className="px-3 py-1 hover:bg-gray-100">Javascript</li>
+                <li className="px-3 py-1 hover:bg-gray-100">How to become a user</li>
                 <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
                   <button className="w-full text-left flex items-center outline-none focus:outline-none">
-                    <span className="pr-1 flex-1">Python</span>
-                    <span className="mr-auto">
-                      <svg
-                        className="fill-current h-4 w-4
-                transition duration-150 ease-in-out"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                      </svg>
-                    </span>
+                    <span className="pr-1 flex-1 uppercase">Escrow Account</span>
                   </button>
-                  <ul
-                    className="bg-white border rounded-sm absolute top-0 right-0 
-      transition duration-150 ease-in-out origin-top-left
-      min-w-32
-      "
-                  >
-                    <li className="px-3 py-1 hover:bg-gray-100">2.7</li>
-                    <li className="px-3 py-1 hover:bg-gray-100">3+</li>
-                  </ul>
                 </li>
-                <li className="px-3 py-1 hover:bg-gray-100">Go</li>
-                <li className="px-3 py-1 hover:bg-gray-100">Rust</li>
               </ul>
             </li>
-            <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Testing</li>
           </ul>
+        </div>
+        <div className="group">
+          <button className=" px-3 py-1 flex items-center min-w-32">
+            <span className="pr-1 font-semibold flex-1 uppercase">Contacts</span>
+            <span>
+              <svg
+                className="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </span>
+          </button>
+        </div>
+        <div className="group">
+          <button className=" px-3 py-1 flex items-center min-w-32">
+            <span className="pr-1 font-semibold flex-1 uppercase">Online bank</span>
+            <span>
+              <svg
+                className="fill-current h-4 w-4 transform group-hover:-rotate-180
+        transition duration-150 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </span>
+          </button>
         </div>
       </ul>
     </nav>
