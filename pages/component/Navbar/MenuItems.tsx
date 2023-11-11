544a3d8bc4441595302cb212997fdeb37d6c4a57
David Waza <wazabanga00@gmail.com>
2023-11-11 22:39:46 +0100

    created dropdown menu

--- pages/component/Navbar/MenuItems.tsx
+++ pages/component/Navbar/MenuItems.tsx
@@ -1,14 +1,29 @@
 export const menuItems = [
-    {
-      title: 'Home',
-      url: '/',
-    },
-    {
-      title: 'Services',
-      url: '/services',
-    },
-    {
-      title: 'About',
-      url: '/about',
-    },
-  ];
+  {
+    title: "Home",
+    url: "/",
+  },
+
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Services",
+    url: "/services",
+    submenu: [
+      {
+        title: "web design",
+        url: "web-design",
+      },
+      {
+        title: "web development",
+        url: "web-dev",
+      },
+      {
+        title: "SEO",
+        url: "seo",
+      },
+    ],
+  },
+];
