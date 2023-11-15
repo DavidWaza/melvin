bc288f6a5d2ca783938aabb8114032a800d504c5
David Waza <wazabanga00@gmail.com>
2023-11-15 14:59:33 +0100

    added fixed social icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 import Carousel from "../component/Carousel/Carousel";
 import Container from "../component/Container/Container";
 import ExchangeRate from "../component/ExchangeComponent/ExchangeRate";
 import BodyText from "../component/BodyText/BodyText";
 import Slider from "../component/Slider/Slider";
-import Footer from "../component/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +12,9 @@ export default function Home() {
   return (
     <main className={`${inter.className}`}>
       <Carousel />
-      <Container>
+      <Container className="relative">
         <ExchangeRate />
+       
         <Slider />
       </Container>
       <BodyText />
