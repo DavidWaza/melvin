537b73c94eb31ec98859783e2705f88f7d44f591
David Waza <wazabanga00@gmail.com>
2023-12-03 14:21:31 +0100

    added bg to available bal

--- src/pages/dashboard/components/CashAvailable.tsx
+++ src/pages/dashboard/components/CashAvailable.tsx
@@ -3,9 +3,10 @@ import { Text } from "@/src/component/Typhography/Typography";
 
 const CashAvailable = () => {
   return (
-    <div>
+    <div className="bg-slate-800 p-5 lg:p-10 lg:w-[500px] w-full rounded-sm">
+      
       <Text variant="extrasmall" className="text-white">
-        Amount Available
+        Available Balance
       </Text>
       <Text variant="large" className="text-white">
         $ 0.00
